edbe5752d0061c6224cf769058838aae59ce59c3
Shinigami <83348119+insurrection-act@users.noreply.github.com>
2022-07-11 12:49:52 +0430

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install python3
+sudo apt-get install python3-pip
+pip3 install termcolor
+pip3 install pycryptodome
+sudo apt-get install g++-mingw-w64-x86-64
+sudo apt-get install mingw-w64
+sudo ln -s /usr/bin/x86_64-w64-mingw32-windres /usr/bin/windres
